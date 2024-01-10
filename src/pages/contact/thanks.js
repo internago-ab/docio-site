8aaf398371537431e67cadf8584417b9151f9221
Matilda Björk <matildabjork@MacBook-Pro-som-tillhor-Matilda.local>
2024-01-10 21:33:36 +0100

    minor fixes

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import "../../components/thank-you.css"
 
 // eslint-disable-next-line
 export default () => (
   <Layout>
-    <section className="section">
-      <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+     <section className="section ">
+      <div className="main-thank">
+        <div className="card">
+          <h2 className="heading">Thank you!</h2>
+          <p className="paragraph">Your form submission has been received.</p>
+          <div className="cta-btn btn-thank">
+            <a href="/">← Back to our site</a>
+          </div>
         </div>
       </div>
     </section>
