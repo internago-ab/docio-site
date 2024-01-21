37f1ced0a1ad729d5abb0ac875bf57a0bd532a81
Matilda Björk <matildabjork@MacBook-Pro-som-tillhor-Matilda.local>
2024-01-21 20:50:17 +0100

    cms config

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { ProductPageTemplate } from '../../templates/product-page'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ProductPageTemplate } from '../../templates/product-page';
 
 const ProductPagePreview = ({ entry, getAsset }) => {
-  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
-  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
 
-  const entryTestimonials = entry.getIn(['data', 'testimonials'])
-  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
-
-  const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
-  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
+  // Retrieve the 'categories' instead of 'plans'
+  const entryPricingCategories = entry.getIn(['data', 'pricing', 'categories']);
+  const pricingCategories = entryPricingCategories ? entryPricingCategories.toJS() : [];
 
   return (
     <ProductPageTemplate
@@ -21,17 +17,17 @@ const ProductPagePreview = ({ entry, getAsset }) => {
       pricing={{
         heading: entry.getIn(['data', 'pricing', 'heading']),
         description: entry.getIn(['data', 'pricing', 'description']),
-        plans: pricingPlans,
+        categories: pricingCategories, 
       }}
     />
-  )
-}
+  );
+};
 
 ProductPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   getAsset: PropTypes.func,
-}
+};
 
-export default ProductPagePreview
+export default ProductPagePreview;
