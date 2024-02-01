6a11b18654527b83ce6dd5c98c6fc2a3be6bcc17
Matilda Björk <matildabjork@MacBook-Pro-som-tillhor-Matilda.local>
2024-02-01 17:33:33 +0100

    more styling for qa

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,7 +5,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
-import QaPagePreview from './preview-templates/qaPagePreview'
+import QaPagePreview from './preview-templates/QaPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
--- src/components/QuestionsAnswers.js
+++ src/components/QuestionsAnswers.js
@@ -13,48 +13,62 @@ const QuestionsAnswers = ({ categories }) => {
   };
   useEffect(() => {
     // Initialize displayedPlans with all questions
-    const allQuestions = categories.flatMap(category =>
-      category.questions.map(question => ({
+    const allQuestions = categories.flatMap((category) =>
+      category.questions.map((question) => ({
         ...question,
         categoryNames: category.categoryNames, // Assuming these are arrays
-        countryNames: category.countryNames   // Assuming these are arrays
-      }))
+        countryNames: category.countryNames, // Assuming these are arrays
+      })),
     );
     setDisplayedPlans(allQuestions);
   }, [categories]);
 
   useEffect(() => {
     // Filter displayedPlans based on selectedCategory and selectedCountry
-    let filtered = categories.flatMap(category =>
-      category.questions.map(question => ({
+    let filtered = categories.flatMap((category) =>
+      category.questions.map((question) => ({
         ...question,
         categoryNames: category.categoryNames,
-        countryNames: category.countryNames
-      }))
+        countryNames: category.countryNames,
+      })),
     );
 
     if (selectedCategory) {
-      filtered = filtered.filter(question => question.categoryNames.includes(selectedCategory));
+      filtered = filtered.filter((question) =>
+        question.categoryNames.includes(selectedCategory),
+      );
     }
 
     if (selectedCountry) {
-      filtered = filtered.filter(question => question.countryNames.includes(selectedCountry));
+      filtered = filtered.filter((question) =>
+        question.countryNames.includes(selectedCountry),
+      );
     }
 
     if (searchQuery) {
-      filtered = filtered.filter(question =>
-        question.answer.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        question.description.toLowerCase().includes(searchQuery.toLowerCase())
+      filtered = filtered.filter(
+        (question) =>
+          question.answer.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          question.description
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase()),
       );
     }
 
     setDisplayedPlans(filtered);
   }, [selectedCategory, selectedCountry, searchQuery, categories]); // Removed displayedPlans from dependencies
 
-  const uniqueCategoryNames = Array.from(new Set(categories.flatMap(category => category.categoryNames).filter(Boolean)));
-  const uniqueCountryNames = Array.from(new Set(categories.flatMap(category => category.countryNames).filter(Boolean)));
+  const uniqueCategoryNames = Array.from(
+    new Set(
+      categories.flatMap((category) => category.categoryNames).filter(Boolean),
+    ),
+  );
+  const uniqueCountryNames = Array.from(
+    new Set(
+      categories.flatMap((category) => category.countryNames).filter(Boolean),
+    ),
+  );
 
-  
   const handleCategoryChange = (categoryName) => {
     setSelectedCategory(categoryName);
     setSelectedCountry(""); // Reset country filter when changing category
@@ -69,67 +83,76 @@ const QuestionsAnswers = ({ categories }) => {
     setSearchQuery(event.target.value);
   };
 
-
   return (
     <div>
-      {/* Search Bar */}
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search for questions"
-          value={searchQuery}
-          onChange={handleSearchChange}
-        />
-      </div>
       <div className="questions_answers-content">
         <div>
-      <h3>Categories:</h3>
-      <div className="category-buttons filter-tags">
-        {uniqueCategoryNames.map((categoryName) => (
-          <button
-            key={categoryName}
-            onClick={() => handleCategoryChange(categoryName)}
-            className={`button ${selectedCategory === categoryName ? "is-selected" : ""}`}
-          >
-            {categoryName}
-          </button>
-        ))}
-      </div>
-
-      <h3 className="country-buttons-header">Countries</h3>
-      <div className="country-buttons filter-tags">
-        {uniqueCountryNames.map((countryName) => (
-          <button
-            key={countryName}
-            onClick={() => handleCountryChange(countryName)}
-            className={`button ${selectedCountry === countryName ? "is-selected" : ""}`}
-          >
-            {countryName}
-          </button>
-        ))}
-      </div>
-      </div>
-      <div className="questions_answers-grid">
-      <div className="text">
-  {displayedPlans.map((plan, index) => (
-    <div key={index} className={` questions_answers-card ${activeIndex === index ? 'expanded' : 'collapsed'}`}>
-      <button className="qa-btn" onClick={() => toggleVisibility(index)}>
-        <span>{activeIndex === index ? '-' : '+'}</span>
-        {plan.answer}
-      </button>
-      {activeIndex === index && (
-        <div className="questions_answers_tab">
-          <p className="prices-paragraph">{plan.description}</p>
-          <div className="price-answer">
-            <p>{plan.subHeader}</p>
+          <div className="categories-filter">
+            <h3>Categories:</h3>
+            <div className="category-buttons filter-tags">
+              {uniqueCategoryNames.map((categoryName) => (
+                <button
+                  key={categoryName}
+                  onClick={() => handleCategoryChange(categoryName)}
+                  className={`button ${selectedCategory === categoryName ? "is-selected" : ""}`}
+                >
+                  {categoryName}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div className="countries-filter">
+            <h3 className="country-buttons-header">Countries</h3>
+            <div className="country-buttons filter-tags">
+              {uniqueCountryNames.map((countryName) => (
+                <button
+                  key={countryName}
+                  onClick={() => handleCountryChange(countryName)}
+                  className={`button ${selectedCountry === countryName ? "is-selected" : ""}`}
+                >
+                  {countryName}
+                </button>
+              ))}
+            </div>
           </div>
         </div>
-      )}
-    </div>
-  ))}
-</div>
-
-      </div>
+        <div className="questions_answers-grid">
+          {/* Search Bar */}
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search for questions.."
+              value={searchQuery}
+              onChange={handleSearchChange}
+            />
+          </div>
+          <div className="text">
+            {displayedPlans.map((plan, index) => (
+              <div
+                key={index}
+                className={` questions_answers-card ${activeIndex === index ? "expanded" : "collapsed"}`}
+              >
+                <button
+                  className="qa-btn"
+                  onClick={() => toggleVisibility(index)}
+                >
+                  <span>{activeIndex === index ? "-" : "+"}</span>
+                  {plan.answer}
+                </button>
+                {activeIndex === index && (
+                  <div className="questions_answers_tab">
+                    <p className="questions_answers_paragraph">
+                      {plan.description}
+                    </p>
+                    <div className="price-answer">
+                      <p>{plan.subHeader}</p>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
