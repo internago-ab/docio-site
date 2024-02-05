c8849568e42ad11508349e02ce9892a11534f825
Matilda Björk <matildabjork@MBPsomtrMatilda.localdomain>
2024-02-05 12:52:17 +0100

    image fix for about

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { AboutPageTemplate } from '../../templates/about-page'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { AboutPageTemplate } from '../../templates/about-page';
 
 const AboutPagePreview = ({ entry, widgetFor, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
-  const entryTestimonials = entry.getIn(['data', 'testimonials'])
-  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
+  const data = entry.getIn(['data']).toJS(); // This converts the entire data object into a JS object
+  const entryTestimonials = entry.getIn(['data', 'testimonials']);
+  const testimonials = entryTestimonials ? entryTestimonials.toJS() : [];
 
-  return(
-  <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
-    mainpitch={data.mainpitch || {}}
-    testimonials={testimonials}
-    fullImage={getAsset(entry.getIn(['data', 'full_image']))}
-  />
-  )
-}
+  // Check if mainpitch has an image and replace it with the correct URL
+  if (data.mainpitch && data.mainpitch.image) {
+    const imagePath = getAsset(data.mainpitch.image);
+    data.mainpitch.image = imagePath.toString();
+  }
+
+  return (
+    <AboutPageTemplate
+      title={entry.getIn(['data', 'title'])}
+      content={widgetFor('body')}
+      mainpitch={data.mainpitch}
+      testimonials={testimonials}
+      fullImage={getAsset(entry.getIn(['data', 'full_image']))}
+    />
+  );
+};
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
@@ -24,7 +30,6 @@ AboutPagePreview.propTypes = {
   }),
   widgetFor: PropTypes.func,
   getAsset: PropTypes.func,
+};
 
-}
-
-export default AboutPagePreview
+export default AboutPagePreview;
